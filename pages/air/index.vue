8ff5c983f77676f65259b52c0af73ee57bd893cc
gzkfree <gzkfree@163.com>
2019-06-25 20:44:56 +0800

    初始化工作

---
+++ pages/air/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    飞机票
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   activated() {
+   }
+}
+</script>
+<style lang='less' scoped>
+</style>
